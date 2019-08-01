13f561758f61012952a3930f148af372e004b213
Felix Cornelissen <felix@icatt.nl>
2019-08-01 19:33:39 +0200

    frontend in buildscript

--- all.bat
+++ all.bat
@@ -4,4 +4,5 @@ start watch.run .\src\CurveRecipes\CurveRecipes.Query.Service &
 start watch.run .\src\MarketCurves\MarketCurves.Service &
 start watch.run .\src\MarketCurves\MarketCurves.Query.Service &
 start watch.run .\src\Instruments\Instruments.Service &
-start watch.run .\src\Instruments\Instruments.Query.Service
+start watch.run .\src\Instruments\Instruments.Query.Service &
+start npm run --prefix .\src\Presentation\MicroFrontends\curve-tool serve -- --port 8081
