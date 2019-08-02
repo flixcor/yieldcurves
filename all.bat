f3596c0f8a3be1cc28c2666217884d47e6e727bd
Felix Cornelissen <felix@icatt.nl>
2019-08-02 16:29:58 +0200

    update build script to run all dependencies

--- all.bat
+++ all.bat
@@ -5,4 +5,10 @@ start watch.run .\src\MarketCurves\MarketCurves.Service &
 start watch.run .\src\MarketCurves\MarketCurves.Query.Service &
 start watch.run .\src\Instruments\Instruments.Service &
 start watch.run .\src\Instruments\Instruments.Query.Service &
-start npm run --prefix .\src\Presentation\MicroFrontends\curve-tool serve -- --port 8081
+start npm run --prefix .\src\Presentation\MicroFrontends\curve-tool serve -- --port 8081 &
+start npm run --prefix .\src\CurveRecipes\CurveRecipes.Service\client-app build &
+start npm run --prefix .\src\CurveRecipes\CurveRecipes.Query.Service\client-app build &
+start npm run --prefix .\src\MarketCurves\MarketCurves.Service\client-app build &
+start npm run --prefix .\src\MarketCurves\MarketCurves.Query.Service\client-app build &
+start npm run --prefix .\src\Instruments\Instruments.Service\client-app build &
+start npm run --prefix .\src\Instruments\Instruments.Query.Service\client-app build
