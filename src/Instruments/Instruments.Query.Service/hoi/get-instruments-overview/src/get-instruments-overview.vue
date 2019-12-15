28d12514e2648a54215ddc0b86fd5757dba338f2
Felix Cornelissen <felixcornelissen@gmail.com>
2019-12-15 23:52:43 +0100

    experiment met rollup

---
+++ src/Instruments/Instruments.Query.Service/hoi/get-instruments-overview/src/get-instruments-overview.vue
@@ -0,0 +1,50 @@
+<template>
+  <ct-card>
+    <template v-slot:title>
+      <span>Instruments</span>
+      <ct-spacer />
+      <ct-btn
+        class="primary"
+        fab
+        @click="onCreateClick"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </ct-btn>
+    </template>
+
+    <template v-slot:content>
+      <ct-table>
+        <thead>
+          <tr>
+            <th>Vendor</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="instrument of entities"
+            :key="instrument.id"
+          >
+            <td>{{ instrument.vendor }}</td>
+            <td>{{ instrument.description }}</td>
+          </tr>
+        </tbody>
+      </ct-table>
+    </template>
+  </ct-card>
+</template>
+<script>
+export default {
+  props: {
+    entities: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    onCreateClick() {
+      this.$emit("create");
+    }
+  }
+};
+</script>
