1be2a1907298f793b40e0353858bdc556b87acde
Felix Cornelissen <felix@icatt.nl>
2019-12-23 14:44:01 +0100

    cleanup

--- src/Common/Common.EventStore/EventController.cs
+++ src/Common/Common.EventStore/EventController.cs
@@ -77,22 +77,4 @@ namespace Common.EventStore.Controllers
             _logger.LogInformation("done");
         }
     }
-
-    internal static class TaskHelper
-    {
-        #region Methods
-        internal static Task WaitAsync(this CancellationToken cancellationToken)
-        {
-            TaskCompletionSource<bool> cancelationTaskCompletionSource = new TaskCompletionSource<bool>();
-            cancellationToken.Register(CancellationTokenCallback, cancelationTaskCompletionSource);
-
-            return cancellationToken.IsCancellationRequested ? Task.CompletedTask : cancelationTaskCompletionSource.Task;
-        }
-
-        private static void CancellationTokenCallback(object taskCompletionSource)
-        {
-            ((TaskCompletionSource<bool>)taskCompletionSource).SetResult(true);
-        }
-        #endregion
-    }
 }
