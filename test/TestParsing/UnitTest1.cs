2aaa518539d11416a0ce190b60402095aafc518e
Felix Cornelissen <felixcornelissen@gmail.com>
2020-06-06 17:31:37 +0200

    more playing around with reflextion

--- test/TestParsing/UnitTest1.cs
+++ test/TestParsing/UnitTest1.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using Akka.Dispatch.SysMsg;
 using Common.Core;
@@ -15,10 +16,10 @@ namespace TestParsing
         [Fact]
         public void Test1()
         {
-            var json = "{ \"maturity\": 5}";
+            var json = "{ \"maturity\": 5, \"somethingElse\": \"etc\"}";
             var obj = JsonDocument.Parse(json);
 
-            var result = obj.TryParse<double, Maturity>(Maturity.TryCreate);
+            var result = obj.TryParse((double d) => Maturity.TryCreate(d));
 
             if (result is Right<Error, Maturity> right)
             {
@@ -30,6 +31,35 @@ namespace TestParsing
                 Assert.True(false);
             }
         }
+
+        [Fact]
+        public void Test2()
+        {
+            var json = "{ \"maturity\": 5, \"somethingElse\": \"etc\"}";
+            var obj = JsonDocument.Parse(json);
+
+            var result = obj.TryParse((string s) => new SomethingElse(s));
+
+            if (result is Right<Error, SomethingElse> right)
+            {
+                var somethingElse = (SomethingElse)right;
+                Assert.Equal("etc", somethingElse.Value);
+            }
+            else
+            {
+                Assert.True(false);
+            }
+        }
+    }
+
+    public class SomethingElse
+    {
+        public SomethingElse(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
     }
 
     public static class Extensions
@@ -50,10 +80,17 @@ namespace TestParsing
             Func<T1, T2, Either<Error, TOut>> exp)
                 => document.TryParse<TOut>(exp);
 
+        public static Either<Error, TOut> TryParse<T1, TOut>(
+            this JsonDocument document,
+            Func<T1, TOut> exp)
+                => document.TryParse<TOut>(exp);
+
 
 
         private static Either<Error, TOut> FromJsonDocument<TOut>(MethodInfo methodInfo, JsonDocument document)
         {
+            var methodParams = methodInfo.GetParameters().ToArray();
+
             var parameters = methodInfo.GetParameters().Select(x => GetParameter(typeof(TOut), x, document)).ToArray();
 
             var errors = parameters.OfType<Left<Error, dynamic>>().SelectMany(x => x.Value.Messages).ToArray();
@@ -65,11 +102,56 @@ namespace TestParsing
 
             var succesParameters = parameters.OfType<Right<Error, dynamic>>().Select(x => x.Value).ToArray();
 
-            var result = methodInfo.Invoke(null, succesParameters);
+            object result = null;
+
+            if (methodInfo.IsStatic)
+            {
+                result = methodInfo.Invoke(null, succesParameters);
+            }
+            else
+            {
+                var paramterless = typeof(TOut).GetConstructors().SingleOrDefault(c => c.GetParameters().Count() == 0);
+
+                if (paramterless != null)
+                {
+                    var target = paramterless.Invoke(null);
+                    result = methodInfo.Invoke(target, succesParameters);
+                }
+                else
+                {
+                    var matching = typeof(TOut).GetConstructors().SingleOrDefault(c =>
+                    {
+                        var consParams = c.GetParameters().ToArray();
+                        var length = consParams.Length;
+                        for (int i = 0; i < length; i++)
+                        {
+                            var consParam = consParams[i];
+                            var methodParam = methodParams[i];
+
+                            if (consParam.ParameterType != methodParam.ParameterType)
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    });
+
+                    if (matching != null)
+                    {
+                        result = matching.Invoke(succesParameters);
+                    }
+                }
+            }
+
 
             if (result is Either<Error, TOut> either)
             {
                 return either;
+            }
+
+            if (result is TOut tOut)
+            {
+                return tOut;
             }
 
             return new Error("something went wrong");
@@ -86,6 +168,11 @@ namespace TestParsing
                 {
                     return new Error("Not found");
                 }
+            }
+
+            if (parameterInfo.ParameterType == typeof(string))
+            {
+                return (dynamic)jsonParam.GetString();
             }
 
             if (parameterInfo.ParameterType == typeof(double))
