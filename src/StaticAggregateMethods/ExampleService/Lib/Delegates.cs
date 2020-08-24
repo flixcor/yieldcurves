c15ae06c0d3fefba31f5f2b07c2853b23597a9b9
Felix Cornelissen <felixcornelissen@gmail.com>
2020-08-24 19:17:43 +0200

    cleanup

--- src/StaticAggregateMethods/ExampleService/Lib/Delegates.cs
+++ src/StaticAggregateMethods/ExampleService/Lib/Delegates.cs
@@ -1,8 +1,10 @@
-﻿namespace ExampleService.Shared
+﻿using System.Collections.Generic;
+
+namespace ExampleService.Shared
 {
     public static class Delegates
     {
-        public delegate object[] Handle<State, Command>(State state, Command command);
+        public delegate IEnumerable<object> Handle<State, Command>(State state, Command command);
         public delegate State When<State, Event>(State state, Event @event);
         public delegate string GetStreamName(string id);
     }
