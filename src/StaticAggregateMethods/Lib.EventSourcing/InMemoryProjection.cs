c1fab3a17cbc1776a715b6349b96d5bd5f6663c5
Felix Cornelissen <felixcornelissen@gmail.com>
2020-09-09 14:15:30 +0200

    always update position

--- src/StaticAggregateMethods/Lib.EventSourcing/InMemoryProjection.cs
+++ src/StaticAggregateMethods/Lib.EventSourcing/InMemoryProjection.cs
@@ -1,28 +1,32 @@
 ﻿using System;
+using System.Linq;
 using Projac;
 
 namespace Lib.EventSourcing
 {
     public abstract class InMemoryProjection<T> : Projection<InMemoryProjectionStore> where T : class, new()
     {
+        protected InMemoryProjection() => 
+            When<EventEnvelope>((mem, e) =>
+            {
+                if (!Handlers.Any(x=> x.Message == e.GetType()))
+                {
+                    mem.UpdatePosition<T>(e.Position);
+                }
+            });
+
         protected void CreateOrUpdateWhen<E>(Func<EventEnvelope<E>, T, T> mapper, Func<EventEnvelope<E>, string>? getId = null) where E : class
         {
-            getId ??= (e) => e.AggregateId;
+            getId ??= (e) => e.AggregateId ?? throw new Exception();
 
-            When<EventEnvelope<E>>((mem, e) =>
-            {
-                mem.AddOrUpdate<T>(e.Position, (t) => mapper(e, t), getId(e));
-            });
+            When<EventEnvelope<E>>((mem, e) => mem.AddOrUpdate<T>(e.Position, (t) => mapper(e, t), getId(e)));
         }
 
         protected void DeleteWhen<E>(Func<EventEnvelope<E>, string>? getId = null) where E : class
         {
-            getId ??= (e) => e.AggregateId;
+            getId ??= (e) => e.AggregateId ?? throw new Exception();
 
-            When<EventEnvelope<E>>((mem, e) =>
-            {
-                mem.Delete<E>(e.Position, getId(e));
-            });
+            When<EventEnvelope<E>>((mem, e) => mem.Delete<T>(e.Position, getId(e)));
         }
     }
 }
--- src/StaticAggregateMethods/Lib.EventSourcing/InMemoryProjectionStore.cs
+++ src/StaticAggregateMethods/Lib.EventSourcing/InMemoryProjectionStore.cs
@@ -11,9 +11,9 @@ namespace Lib.EventSourcing
 
         private InMemoryProjectionStore() { }
 
-        private readonly ConcurrentDictionary<Type, (long, Dictionary<string, object>)> _projections = new ConcurrentDictionary<Type, (long, Dictionary<string, object>)>();
+        private readonly ConcurrentDictionary<Type, (long, Dictionary<string, object?>)> _projections = new ConcurrentDictionary<Type, (long, Dictionary<string, object?>)>();
 
-        public (long, T) Get<T>(string id) where T : class =>
+        public (long, T?) Get<T>(string id) where T : class =>
             _projections.TryGetValue(typeof(T), out var projection)
                 ? projection.Item2.TryGetValue(id, out var obj) && obj is T t
                     ? (projection.Item1, t)
@@ -27,17 +27,25 @@ namespace Lib.EventSourcing
         public void AddOrUpdate<T>(long version, Func<T, T> mapper, string id) where T : class, new () 
         => _projections.AddOrUpdate(
             typeof(T),
-            _ => (version, new Dictionary<string, object> { { id, mapper(new T()) } }),
+            _ => (version, new Dictionary<string, object?> { { id, mapper(new T()) } }),
             (_, tup) => version > tup.Item1 
-                ? (version, new Dictionary<string, object>(tup.Item2) { [id] = mapper(tup.Item2.TryGetValue(id, out var val) && val is T t ? t : new T()) })
+                ? (version, new Dictionary<string, object?>(tup.Item2) { [id] = mapper(tup.Item2.TryGetValue(id, out var val) && val is T t ? t : new T()) })
                 : tup
             );
 
         public void Delete<T>(long version, string id) => _projections.AddOrUpdate(
             typeof(T),
-            _ => (version, new Dictionary<string, object>()),
+            _ => (version, new Dictionary<string, object?>()),
             (_, tup) => version > tup.Item1
-                ? (version, new Dictionary<string, object>(tup.Item2) { [id] = null })
+                ? (version, new Dictionary<string, object?>(tup.Item2) { [id] = null })
+                : tup
+            );
+
+        public void UpdatePosition<T>(long version) => _projections.AddOrUpdate(
+            typeof(T),
+            _ => (version, new Dictionary<string, object?>()),
+            (_, tup) => version > tup.Item1
+                ? (version, tup.Item2)
                 : tup
             );
     }
